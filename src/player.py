db1ee084f16ab4616438f4e7fce7b3ead81d13f4
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2024-10-30 14:03:21 +0500

    Update player.py

--- src/player.py
+++ src/player.py
@@ -7,21 +7,13 @@ from src.constants import Direction, TankState
 
 class Player(Tank):
     def __init__(self, game, level, type, position=None, direction=None, filename=None):
-
         Tank.__init__(self, game, level, type, position=None, direction=None)
-
         if filename is None:
             filename = (0, 0, 16 * 2, 16 * 2)
-
         self.start_position = position
         self.start_direction = direction
-
         self.lives = 3
-
-        # total score
         self.score = 0
-
-        # store how many bonuses in this stage this player has collected
         self.trophies = {
             "bonus": 0,
             "enemy0": 0,
@@ -29,7 +21,6 @@ class Player(Tank):
             "enemy2": 0,
             "enemy3": 0
         }
-
         self.image = self.game.sprites.subsurface(filename)
         self.image_up = self.image
         self.image_left = pygame.transform.rotate(self.image, 90)
@@ -42,8 +33,6 @@ class Player(Tank):
             self.rotate(direction, False)
 
     def move(self, direction):
-        """ move player if possible """
-
         if self.state == TankState.Exploding:
             if not self.explosion.active:
                 self.state = TankState.Dead
@@ -52,7 +41,6 @@ class Player(Tank):
         if self.state != TankState.Alive:
             return
 
-        # rotate player
         if self.direction != direction:
             self.rotate(direction)
 
@@ -60,7 +48,6 @@ class Player(Tank):
             return
 
         new_position = [0, 0]
-        # move player
         if direction == Direction.Up:
             new_position = [self.rect.left, self.rect.top - self.speed]
             if new_position[1] < 0:
@@ -80,30 +67,24 @@ class Player(Tank):
 
         player_rect = pygame.Rect(new_position, [26, 26])
 
-        # collisions with tiles
         if player_rect.collidelist(self.level.obstacle_rects) != -1:
             return
 
-        # collisions with other players
         for player in self.game.players:
-            if player != self and player.state == TankState.Alive and player_rect.colliderect(player.rect) == True:
+            if player != self and player.state == TankState.Alive and player_rect.colliderect(player.rect) is True:
                 return
 
-        # collisions with enemies
         for enemy in self.game.enemies:
-            if player_rect.colliderect(enemy.rect) == True:
+            if player_rect.colliderect(enemy.rect) is True:
                 return
 
-        # collisions with bonuses
         for bonus in self.game.bonuses:
-            if player_rect.colliderect(bonus.rect) == True:
+            if player_rect.colliderect(bonus.rect) is True:
                 self.bonus = bonus
 
-        # if no collision, move player
         self.rect.topleft = (new_position[0], new_position[1])
 
     def reset(self):
-        """ reset player """
         self.rotate(self.start_direction, False)
         self.rect.topleft = self.start_position
         self.superpowers = 0
